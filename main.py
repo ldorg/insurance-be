1680654ff77552555e16c90eed0536c51ff5e150
Logan Donley <ldonley@cloudbees.com>
2021-09-22 13:44:02 -0400

    init version

---
+++ main.py
@@ -0,0 +1,50 @@
+import random
+import shelve
+from typing import Optional
+
+from fastapi import FastAPI, Header
+from pydantic import BaseModel
+
+from enums import ACCOUNT_TYPES
+
+app = FastAPI()
+
+db = shelve.open("db")
+
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to InsuranceHive"}
+
+
+class RegistrationDetails(BaseModel):
+    username: str
+    betaUser: Optional[bool] = False
+    accountType: Optional[str] = None
+
+
+@app.post("/register")
+async def register(registration_details: RegistrationDetails):
+    if registration_details.username in db:
+        return {"message": "Account already exists"}
+    registration_details.accountType = random.choice(ACCOUNT_TYPES)
+    db[registration_details.username] = registration_details
+    return registration_details
+
+
+class LoginDetails(BaseModel):
+    username: str
+
+
+@app.post("/login")
+async def login(login_details: LoginDetails):
+    if login_details.username not in db:
+        return {"error": "User does not exist."}
+    return db[login_details.username]
+
+
+@app.get("/me")
+async def me(authorization: Optional[str] = Header(None)):
+    if authorization:
+        return db[authorization]
+    return {"username": None, "betaUser": None, "accountType": None}
